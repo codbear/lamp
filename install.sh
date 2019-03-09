707865aba600644cda34170542109c6fc632ab40
codbear <douglas.meunier@codbear.com>
2019-03-09 12:32:48 +0100

    feat(install.sh): install apache2

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 # Install some usefull tools
 apt update
 apt install -y net-tools sudo wget curl bash-completion ca-certificates apt-transport-https git
+
+# Install Apache 2
+apt install -y apache2 apache2-doc
+a2enmod rewrite ssl headers
+a2ensite default-ssl.conf
+systemctl restart apache2
