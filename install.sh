d3c3bd7ee2002de6633778822dffa81d5a14db20
codbear <douglas.meunier@codbear.com>
2019-03-09 12:34:13 +0100

    feat(install.sh): install php7.3 and create /var/www/html/info.php

--- install.sh
+++ install.sh
@@ -9,3 +9,11 @@ apt install -y apache2 apache2-doc
 a2enmod rewrite ssl headers
 a2ensite default-ssl.conf
 systemctl restart apache2
+
+# Install php 7.3 and create /var/www/html/info.php
+wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+echo "deb https://packages.sury.org/php/ stretch main" | tee /etc/apt/sources.list.d/php.list
+apt update
+apt upgrade -y
+apt install -y php7.3 php7.3-cli php7.3-common php7.3-curl php7.3-mbstring php7.3-mysql php7.3-xml php7.3-gd php7.3-xmlrpc
+echo "<?php phpinfo(); ?>" | tee /var/www/html/info.php
